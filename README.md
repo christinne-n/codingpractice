9d0fd39b14823f53f26172c03a20f695bf118aa3
christinne-n <christine.nguyen0909@gmail.com>
2025-09-09 18:20:36 -0700

    adding description to README and some notes to practice

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pythonexercises
-python exercises to refresh and review
+This is a repository that I made to practice Python exercises to refresh. 
+I will also use this to take key notes so I can review it when needed.
---
+++ practice.py
@@ -0,0 +1,8 @@
+#.replace == replaces all occurrences of a substring with another substring
+#.upper == converts all characters in a string to uppercase
+#.lower == converts all characters in a string to lowercase
+#.len == returns the length of a string (number of characters)
+
+
+full_name = "John Doe"
+print(full_name.replace("John", "Jane"))  # Output: Jane Doe
