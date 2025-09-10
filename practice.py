bfeb9c42eb89c0a3aaca72377af0530a7b0d1782
christinne-n <christine.nguyen0909@gmail.com>
2025-09-09 20:16:07 -0700

    adding more notes and practice

--- practice.py
+++ practice.py
@@ -2,7 +2,13 @@
 #.upper == converts all characters in a string to uppercase
 #.lower == converts all characters in a string to lowercase
 #.len == returns the length of a string (number of characters)
-
+#multiline strings == use triple quotes (''' or """) to create strings that span multiple lines
+#slices == use [start:end] to extract a substring from a string (start index is inclusive, end index is exclusive)
 
 full_name = "John Doe"
 print(full_name.replace("John", "Jane"))  # Output: Jane Doe
+print("What is your name?")
+my_name=input()
+print("Hello " + my_name)
+print(my_name[4])
+print(my_name[0:3])
